ee79c2bdc329c2e091481fa82c7f0eb7eb1d7f4d
novfps <arthur_mello@outlook.com.br>
2024-10-03 19:24:34 -0300

    Add files via upload

---
+++ atividadeatletas/dados.js
@@ -0,0 +1,22 @@
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descricao: "Rayssa Leal, a Fadinha, é uma skatista brasileira",
+        link:"https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate fada sk8"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que se...",
+        link:"https://pt.wikipedia.org/wiki/Beatriz Souza",
+        tags: "judô judo judoca judoka"
+
+    },
+    {       
+    titulo: "Rebeca Andrade",
+    descricao: "Rebeca Andrade é uma ginasta brasileira",
+    link:"https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+    tags: "ginástica ginastica ouro medalha"
+
+    }
+];
